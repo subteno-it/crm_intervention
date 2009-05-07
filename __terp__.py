53ad1c1cd7b93539bcfe6c68145820ed0c98276b
Sebastien LANGE <alnslang@ns205928.ovh.net>
2009-05-07 21:00:31 +0200

    [ADD]  * add crm_intervention module

---
+++ __terp__.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution	
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Customer Relationship Management',
+    'version': '1.0',
+    'category': 'Generic Modules/CRM & SRM',
+    'description': """
+    TODO
+""",
+    'author': 'Syleam',
+    'website': 'http://www.Syleam.fr',
+    'depends': ['crm','crm_configuration'],
+    'init_xml': [
+        #        'crm_configuration_wizard.xml',
+        'crm_intervention_data.xml',
+        'crm_intervention_view.xml',
+        'crm_intervention_menu.xml',
+    ],
+    'update_xml': [],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ crm_intervention.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution    
+#    Copyright (C) 2009 SYLEAM
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+#   AIM :
+#           ...
+#
+##############################################################################
+# Date      Author      Description
+# 20090502  SYLEAM/SL   ...
+#
+##############################################################################
+
+
+from osv import fields, osv
+from tools.translate import _
+import time
+
+class crm_intervention_history(osv.osv):
+    _inherit = 'crm.case.history'
+
+    _columns = {
+        'types': fields.char('Type', size=12),
+    }
+
+crm_intervention_history()
+
+class crm_intervention(osv.osv):
+    _inherit = 'crm.case'
+
+    _columns = {
+        'customer_request': fields.text('Customer Request'),
+        'internal_comment': fields.text('Internal Comment'),
+        'history_line': fields.one2many('crm.case.history', 'case_id', 'Communication', readonly=1),
+        'internal_history': fields.one2many('crm.case.history', 'case_id', 'Communication', readonly=1),
+    }
+
+    def case_log(self, cr, uid, ids,context={}, email=False, *args):
+        cases = self.browse(cr, uid, ids)
+        for case in cases:
+            obj = self.pool.get('crm.case.log')
+            data = {
+                'name': _('Historize'),
+                'som': case.som.id,
+                'canal_id': case.canal_id.id,
+                'user_id': uid,
+                'date': time.strftime('%Y-%m-%d %H:%M:%S'),
+                'case_id': case.id,
+                'section_id': case.section_id.id
+            }
+            obj.create(cr, uid, data, context)
+
+            obj = self.pool.get('crm.case.history')
+            data['description'] = case.description
+            data['types'] = 'default'
+            data['email'] = email or \
+                (case.user_id and case.user_id.address_id and \
+                case.user_id.address_id.email) or False
+            obj.create(cr, uid, data, context)
+        return self.write(cr, uid, ids, {'description': False, 'som': False, 'canal_id': False})
+
+    def case_internal_log(self, cr, uid, ids,context={}, email=False, *args):
+        cases = self.browse(cr, uid, ids)
+        for case in cases:
+            obj = self.pool.get('crm.case.log')
+            data = {
+                'name': _('Historize'),
+                'som': case.som.id,
+                'canal_id': case.canal_id.id,
+                'user_id': uid,
+                'date': time.strftime('%Y-%m-%d %H:%M:%S'),
+                'case_id': case.id,
+                'section_id': case.section_id.id
+            }
+            obj.create(cr, uid, data, context)
+
+            obj = self.pool.get('crm.case.history')
+            data['description'] = case.internal_comment
+            data['types'] = 'internal'
+            data['email'] = email or \
+                (case.user_id and case.user_id.address_id and \
+                case.user_id.address_id.email) or False
+            obj.create(cr, uid, data, context)
+        return self.write(cr, uid, ids, {'internal_comment': False, 'som': False, 'canal_id': False})
+
+crm_intervention()
+
