15cc9b591eeda4993d95e74909197cbe2a6d8a6b
Sebastien LANGE <sebastien.lange@syleam.fr>
2016-04-22 17:26:01 +0200

    [FIX] Fix domain in categ_id field

--- crm_intervention.py
+++ crm_intervention.py
@@ -50,7 +50,7 @@ class crm_intervention(models.Model):
     date_closed = fields.Datetime(string='Closed on', readonly=True)
     email_from = fields.Char(string='Email', size=128, states={'done': [('readonly', True)]}, help='These people will receive email.')
     priority = fields.Selection(crm_stage.AVAILABLE_PRIORITIES, default='2', states={'done': [('readonly', True)]})
-    categ_id = fields.Many2one('crm.lead.tag', string='Category', copy=False, domain="[('section_id', '=', section_id), ('object_id.model', '=', 'crm.intervention')]", states={'done': [('readonly', True)]})
+    categ_id = fields.Many2one('crm.lead.tag', string='Category', copy=False, domain="[('team_id', '=', section_id)]", states={'done': [('readonly', True)]})
     number_request = fields.Char(string='Number Request', size=64, default=lambda self: self.env['ir.sequence'].next_by_code('crm.intervention'), states={'done': [('readonly', True)]}, copy=lambda self: self.env['ir.sequence'].next_by_code('crm.intervention'))
     customer_information = fields.Text(string='Customer_information', states={'done': [('readonly', True)]})
     intervention_todo = fields.Text(string='Intervention to do', states={'done': [('readonly', True)]}, help='Indicate the description of this intervention to do')
