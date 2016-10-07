0ccd35d5fff791cd507bf401fdcb5d94ece50e88
Christophe CHAUVET <christophe.chauvet@gmail.com>
2016-10-07 10:46:42 +0200

    [IMP] Retrieve contract when select a customer

--- crm_intervention.py
+++ crm_intervention.py
@@ -240,9 +240,10 @@ class crm_intervention(base_state, base_stage, orm.Model):
             return {
                 'value': {
                     'partner_invoice_id': False, 'partner_shipping_id': False,
-                    'partner_order_id': False, 'email_from':
-                    False, 'partner_address_phone': False,
-                    'partner_address_mobile': False}}
+                    'partner_order_id': False, 'email_from': False,
+                    'partner_address_phone': False, 'partner_address_mobile': False,
+                    'contract_id': False,
+                }}
         addr = self.pool.get('res.partner').address_get(
             cr, uid, [part], ['default', 'delivery', 'invoice', 'contact'])
         part = self.pool.get('res.partner').browse(cr, uid, part)
@@ -257,6 +258,16 @@ class crm_intervention(base_state, base_stage, orm.Model):
             cr, uid, addr['delivery']).phone
         val['partner_address_mobile'] = self.pool.get('res.partner').browse(
             cr, uid, addr['delivery']).mobile
+
+        # retrieve contract if only one
+        ctr_ids = self.pool['account.analytic.account'].search(cr, uid, [
+            ('partner_id', '=', part.id),
+            ('type','=','contract'),
+            ('use_inter', '=', True),
+        ])
+        if len(ctr_ids) == 1:
+            val['contract_id'] = ctr_ids[0]
+
         return {'value': val}
 
     def onchange_planned_duration(self, cr, uid, ids, planned_duration,
