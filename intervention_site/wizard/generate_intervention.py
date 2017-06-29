eda436a7aecd3917e6e8a492cbbdbf462be3df73
Christophe CHAUVET <christophe.chauvet@gmail.com>
2017-06-29 14:26:43 +0200

    Improve wizard generation from equipment
    Remove context to drop filter by user when display list of interventions
    Compute end date for the intervention

--- intervention_site/wizard/generate_intervention.py
+++ intervention_site/wizard/generate_intervention.py
@@ -40,9 +40,13 @@ class GenerateIntervention(orm.TransientModel):
                 'section_id': this.section_id and this.section_id.id or False,
                 'user_id': this.user_id and this.user_id.id or False,
                 'date_planned_start': this.begin_date,
+                'duration_planned': 1.0,
                 'partner_id': part_id,
                 'equipment_id': eq.id,
             }
+            if this.begin_date:
+                int_args['date_planned_end'] = inter_obj.onchange_planned_duration(
+                    cr, uid, [], 1.0, this.begin_date)['value']['date_planned_end']
             int_args.update(part_vals['value'])
             if eq.site_id:
                 int_args['site_id'] = eq.site_id.id
@@ -57,5 +61,6 @@ class GenerateIntervention(orm.TransientModel):
         r_id = result and result[1] or False
         result = act_obj.read(cr, uid, [r_id], context=context)[0]
         result['domain'] = "[('id','in', [" + ','.join(map(str, int_ids)) + "])]"
+        del result['context']
 
         return result
